5deb207240d611ce9ebeb065c7a6a2c975628832
Gobe Hobona <ghobona@opengeospatial.org>
2021-05-25 11:18:52 +0100

    Update VerifyTestNGController.java

--- src/test/java/org/opengis/cite/georss10/VerifyTestNGController.java
+++ src/test/java/org/opengis/cite/georss10/VerifyTestNGController.java
@@ -53,7 +53,7 @@ public class VerifyTestNGController {
 
     @Test
     public void doTestRun() throws Exception {
-        URL testSubject = getClass().getResource("/atom-feed-2.xml");
+        URL testSubject = getClass().getResource("/point_georss.xml");
         this.testRunProps.setProperty(TestRunArg.IUT.toString(), testSubject
                 .toURI().toString());
         ByteArrayOutputStream outStream = new ByteArrayOutputStream(1024);
