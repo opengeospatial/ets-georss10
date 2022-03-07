76c15ef6b5f5575c35bf20a464d45d02b863243b
Gobe Hobona <ghobona@opengeospatial.org>
2022-03-07 15:15:47 +0000

    Update documentation for ETS v0.2
    Fixes https://github.com/opengeospatial/ets-georss10/issues/16

--- src/main/java/org/opengis/cite/georss10/core/Georss11ValidationTests.java
+++ src/main/java/org/opengis/cite/georss10/core/Georss11ValidationTests.java
@@ -28,6 +28,7 @@ import org.opengis.cite.georss10.ErrorMessageKeys;
 import org.opengis.cite.validation.RelaxNGValidator;
 import org.opengis.cite.validation.ValidationErrorHandler;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -37,16 +38,14 @@ public class Georss11ValidationTests extends DataFixture {
 	/**
 	 * Clause 5.1: Use version 1.1 of the GeoRSS schema. The 1.0 version of the schema is still available for backwards compatibility. 
 	 * Clause 7.3: GeoRSS Simple Serialization Test Method: Verify the test subject uses valid where or geometry elements
-	 * Clause 7.4: Encoding of GeoRSS in a GML Profile
 	 *
 	 * @throws SAXException If the resource cannot be parsed.
 	 * @throws IOException  If the resource is not accessible.
 	 */
-	@Test(description = "Implements Clause 5.1 and Clause 7.3 and Clause 7.4 of OGC 17-002r1")
+	@Test(description = "Implements Clause 5.1 and Clause 7.3 of OGC 17-002r1")
 	public void docHasValidSimpleGeoRSSElements() throws SAXException, IOException {
 		
 
-		try {
 
 			URL schemaUrl = this.getClass().getResource("/org/opengis/cite/georss10/xsd/opengis/georss/1.1/georss.xsd");
 
@@ -79,6 +78,58 @@ public class Georss11ValidationTests extends DataFixture {
 					}
 	
 				}
+
+			}
+			else if(testSubject.getElementsByTagNameNS("http://www.georss.org/georss", "where").getLength()>0) {  // GeoRSS GML Conformance
+			
+				throw new SkipException("Detected a GeoRSS GML document. See the results for the docHasValidGMLGeoRSSElements() test.");	
+		
+			}
+			else {
+			
+			assertTrue(geometriesFound,  "There were no georss geometry elements nor where elements found in the document. Fails Clause 7.3 and Clause 7.4" );	
+			
+			}
+			
+	
+
+	}
+	
+	/**
+	 * Clause 5.1: Use version 1.1 of the GeoRSS schema. The 1.0 version of the schema is still available for backwards compatibility. 
+	 * Clause 7.4: Encoding of GeoRSS in a GML Profile
+	 *
+	 * @throws SAXException If the resource cannot be parsed.
+	 * @throws IOException  If the resource is not accessible.
+	 */
+	@Test(description = "Implements Clause 5.1 and Clause 7.4 of OGC 17-002r1")
+	public void docHasValidGMLGeoRSSElements() throws SAXException, IOException {
+		
+
+
+			URL schemaUrl = this.getClass().getResource("/org/opengis/cite/georss10/xsd/opengis/georss/1.1/georss.xsd");
+
+			Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(schemaUrl);
+			Validator validator = schema.newValidator();
+
+		
+			String[] geometryTypes = { "point", "line", "polygon", "box", "elev", "floor", "radius" };
+
+			
+			// check if there are any geometries
+			
+			boolean geometriesFound = false;
+			
+			for (String geometryType : geometryTypes) {
+				NodeList geomList = testSubject.getElementsByTagNameNS("http://www.georss.org/georss", geometryType);
+				int numGeoms = geomList.getLength();				
+				if(numGeoms>0) geometriesFound = true;			
+			}
+						
+			if(geometriesFound) {   //Simple GeoRSS Conformance
+				System.out.println("CHK 1");
+				throw new SkipException("Detected a Simple GeoRSS document. See the results for the docHasValidSimpleGeoRSSElements() test.");	
+				
 
 			}
 			else if(testSubject.getElementsByTagNameNS("http://www.georss.org/georss", "where").getLength()>0) {  // GeoRSS GML Conformance
@@ -130,11 +181,8 @@ public class Georss11ValidationTests extends DataFixture {
 			
 			
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 
-	}
+	}	
 
 	/**
 	 * Clause 6: There are currently two encodings of GeoRSS: Simple and GML...Both
