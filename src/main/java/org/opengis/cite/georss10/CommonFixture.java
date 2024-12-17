9af5508424c7cc2897bb86b45823e1e81df109f4
Benjamin Proß <b.pross@52north.org>
2024-12-17 10:29:56 +0100

    Adjust code

--- src/main/java/org/opengis/cite/georss10/CommonFixture.java
+++ src/main/java/org/opengis/cite/georss10/CommonFixture.java
@@ -1,17 +1,20 @@
 package org.opengis.cite.georss10;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientRequest;
-import com.sun.jersey.api.client.ClientResponse;
 import java.net.URI;
 import java.util.Map;
-import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.client.ClientRequest;
+import org.glassfish.jersey.client.ClientResponse;
 import org.opengis.cite.georss10.util.ClientUtils;
 import org.testng.ITestContext;
 import org.testng.SkipException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.w3c.dom.Document;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 /**
  * A supporting base class that sets up a common test fixture. These
@@ -34,7 +37,7 @@ public class CommonFixture {
     /**
      * An HTTP response message.
      */
-    protected ClientResponse response;
+    protected Response response;
 
     /**
      * Initializes the common test fixture with a client component for 
@@ -73,8 +76,7 @@ public class CommonFixture {
      *
      * @see ClientUtils#getResponseEntityAsDocument
      */
-    public Document getResponseEntityAsDocument(ClientResponse response,
-            String targetURI) {
+    public Document getResponseEntityAsDocument(Response response, String targetURI) {
         return ClientUtils.getResponseEntityAsDocument(response, targetURI);
     }
 
@@ -91,9 +93,8 @@ public class CommonFixture {
      *
      * @see ClientUtils#buildGetRequest
      */
-    public ClientRequest buildGetRequest(URI endpoint,
-            Map<String, String> qryParams, MediaType... mediaTypes) {
+    public Response buildGetRequest(URI endpoint, Map<String, String> qryParams, MediaType... mediaTypes) {
         return ClientUtils.buildGetRequest(endpoint, qryParams, mediaTypes);
-    }
+}
 
 }
